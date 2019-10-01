f19c01436a4aa999260038e1b2d02898783d1ce1
Jeremy Wiebe <jeremyw76@live.com>
2019-10-01 11:32:29 -0500

    Rails Challenge
    - The user should be able to maintain a blog by performing CRUD
    operations on blog articles using a web interface.
    - Created an article model with constraints to ensure that each article has a
    valid title.
    - Created a controller with actions and views for each of the CRUD operations on articles.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,43 @@
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :text))
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(params.require(:article).permit(:title, :text))
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+end
